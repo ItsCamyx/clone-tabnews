361577eaa522ced3af3c8bb4650043a40963c3ac
Camila Correia <96155623+ItsCamyx@users.noreply.github.com>
2026-02-23 14:32:01 -0300

    adicionando nova mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Se vocë me ama, dê uma risadinha!</h1>
+            <h1>Se você me ama, dê uma risadinha!</h1>
         </div>
     );
 }
