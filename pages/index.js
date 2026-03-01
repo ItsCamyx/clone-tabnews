28b7f00f534d9489e08b99ab12c20734fa60b85c
Camila Correia <96155623+ItsCamyx@users.noreply.github.com>
2026-03-01 18:41:36 +0000

    Ajuste de lint

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <div>
-            <h1>Se você me ama, dê uma risadinha!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Se você me ama, dê uma risadinha!</h1>
+    </div>
+  );
 }
 
 export default Home;
